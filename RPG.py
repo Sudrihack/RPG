c2be48ed7089ba0a454a668d43c771e1cad9a8b1
Guillaume Roux <guiguionthenet@free.fr>
2020-07-10 10:31:06 +0200

    Ajout d'une action ne respectant pas le format. Correction bugs.

--- RPG.py
+++ RPG.py
@@ -1,13 +1,14 @@
 # coding: utf-8
 
+def valider_colonnes(ligne):
+    colonnes = ligne.split(',')
+    return len(colonnes) == 3 # magick number
+
 with open('actions.csv') as f:
     ligne = f.readline()
     while ligne:
-        print(ligne, end='')
+        print(ligne[:-1]) # faire quelque chose (dernier char = \n)
         if not valider_colonnes(ligne):
-            print("Problème avec le nombre de colonnes.")
+            print("Probleme avec le nombre de colonnes.")
         ligne = f.readline()
         
-def valider_colonnes(ligne):
-    colonnes = ligne.split(',')
-    return colonnes == 3 # magick number
