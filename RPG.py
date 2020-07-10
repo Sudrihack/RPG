50c037152cd1fd71a536813ec379cc6be979170b
Guillaume Roux <guiguionthenet@free.fr>
2020-07-10 10:25:09 +0200

    Vérification du nombre de colonnes.

--- RPG.py
+++ RPG.py
@@ -4,5 +4,10 @@ with open('actions.csv') as f:
     ligne = f.readline()
     while ligne:
         print(ligne, end='')
+        if not valider_colonnes(ligne):
+            print("Problème avec le nombre de colonnes.")
         ligne = f.readline()
         
+def valider_colonnes(ligne):
+    colonnes = ligne.split(',')
+    return colonnes == 3 # magick number
