8bb03cf485b35ec37b1d1482d9ee3d63c56a7c58
Guillaume Roux <guiguionthenet@free.fr>
2020-07-10 10:46:34 +0200

    Ajout de la fonction récuperer_ligne. Remarque: je me suis surpris à 'commencer par les coins'. Le prochain commit presentera une version livrable minimale.

--- RPG.py
+++ RPG.py
@@ -1,14 +1,22 @@
 # coding: utf-8
 
 def valider_colonnes(ligne):
-    colonnes = ligne.split(',')
-    return len(colonnes) == 3 # magick number
+    return len(ligne) == 3 # magick number
 
-with open('actions.csv') as f:
-    ligne = f.readline()
-    while ligne:
-        print(ligne[:-1]) # faire quelque chose (dernier char = \n)
-        if not valider_colonnes(ligne):
-            print("Probleme avec le nombre de colonnes.")
+def recuperer_lignes(fichier):
+    lignes = []
+    with open(fichier) as f:
         ligne = f.readline()
+        while ligne:
+            ligne = ligne[:-1].split(',') # faire quelque chose (dernier char = \n); magick char
+            print(ligne)
+            if valider_colonnes(ligne):
+                lignes.append(ligne)
+            else:
+                print("  Probleme avec le nombre de colonnes.")
+            ligne = f.readline()
+
+if __name__ == '__main__':
+    actions = recuperer_lignes('actions.csv') # magick str
+    faits = recuperer_lignes('table_faits.csv')
         
